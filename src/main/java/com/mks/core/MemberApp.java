c6507d9f9fc19a3bf9e39ae73b9e559a5572bbb0
MaKyuSeok <mks502@naver.com>
2021-05-12 10:05:48 +0900

    ADD only java branch

--- src/main/java/com/mks/core/MemberApp.java
+++ src/main/java/com/mks/core/MemberApp.java
@@ -9,7 +9,6 @@ public class MemberApp {
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "membmerA", Grade.VIP);
         memberService.join(member);
 
