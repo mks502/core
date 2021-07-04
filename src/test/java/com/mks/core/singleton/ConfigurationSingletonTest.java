cfb127ee3499e426a0b616f683bdf579baff8e77
MaKyuSeok <mks502@naver.com>
2021-07-04 22:50:53 +0900

    스프링 핵심 원리 - 싱글톤 컨테이너
    싱글톤 컨테이너

---
+++ src/test/java/com/mks/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,38 @@
+package com.mks.core.singleton;
+
+import com.mks.core.AppConfig;
+import com.mks.core.member.MemberRepository;
+import com.mks.core.member.MemberServiceImpl;
+import com.mks.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        assertThat(memberRepository1).isSameAs(memberRepository2);
+        assertThat(memberRepository).isSameAs(memberRepository2);
+    }
+
+    @Test
+    void configurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean.getClass() = " + bean.getClass());
+    }
+}
